c3e15b5ed51fe3802ad54457b2212b5205379800
Kacper Klimczuk <kacper.klimczuk.it@gmail.com>
2024-03-03 13:08:52 +0100

    working deploy

--- src/data/store/courses/courses.state.ts
+++ src/data/store/courses/courses.state.ts
@@ -6,29 +6,29 @@ export interface CoursesState {
 
 export const initializeState = (): CoursesState => {
     return {
-        // courses: {}
-        courses: {
-            1: {
-                id: 1,
-                name: "kurs 1",
-                lastLearningDate: new Date(),
-                nextSuggestedLearningDate: new Date(),
-                lessonIds: [1,2] 
-            },
-            2: {
-                id: 2,
-                name: "kurs 2",
-                lastLearningDate: new Date(),
-                nextSuggestedLearningDate: new Date(),
-                lessonIds: [3] 
-            },
-            3: {
-                id: 3,
-                name: "kurs 3",
-                lastLearningDate: new Date(),
-                nextSuggestedLearningDate: new Date(),
-                lessonIds: [1,2,3,4] 
-            }
-        }
+        courses: {}
+        // courses: {
+        //     1: {
+        //         id: 1,
+        //         name: "kurs 1",
+        //         lastLearningDate: new Date(),
+        //         nextSuggestedLearningDate: new Date(),
+        //         lessonIds: [1,2] 
+        //     },
+        //     2: {
+        //         id: 2,
+        //         name: "kurs 2",
+        //         lastLearningDate: new Date(),
+        //         nextSuggestedLearningDate: new Date(),
+        //         lessonIds: [3] 
+        //     },
+        //     3: {
+        //         id: 3,
+        //         name: "kurs 3",
+        //         lastLearningDate: new Date(),
+        //         nextSuggestedLearningDate: new Date(),
+        //         lessonIds: [1,2,3,4] 
+        //     }
+        // }
     }
 }
--- src/data/store/lessons/lessons.state.ts
+++ src/data/store/lessons/lessons.state.ts
@@ -7,40 +7,40 @@ export interface LessonsState {
 
 export const initializeState = (): LessonsState => {
     return {
-        // lessons: {}
-        lessons: {
-            1: {
-                id: 1,
-                name: "lesson 1",
-                cardIds: [1,2],
-                lastLearningDate: new Date(),
-                nextSuggestedLearningDate: new Date(),
-                noMistakeInARow: 0
-            },
-            2: {
-                id: 2,
-                name: "lesson 2",
-                cardIds: [3,4],
-                lastLearningDate: new Date(),
-                nextSuggestedLearningDate: new Date(),
-                noMistakeInARow: 0
-            },
-            3: {
-                id: 3,
-                name: "lesson 3",
-                cardIds: [5,6],
-                lastLearningDate: new Date(),
-                nextSuggestedLearningDate: new Date(),
-                noMistakeInARow: 0
-            },
-            4: {
-                id: 4,
-                name: "lesson 4",
-                cardIds: [1,2,3,4,5,6],
-                lastLearningDate: new Date(),
-                nextSuggestedLearningDate: new Date(),
-                noMistakeInARow: 0
-            }
-        }
+        lessons: {}
+        // lessons: {
+        //     1: {
+        //         id: 1,
+        //         name: "lesson 1",
+        //         cardIds: [1,2],
+        //         lastLearningDate: new Date(),
+        //         nextSuggestedLearningDate: new Date(),
+        //         noMistakeInARow: 0
+        //     },
+        //     2: {
+        //         id: 2,
+        //         name: "lesson 2",
+        //         cardIds: [3,4],
+        //         lastLearningDate: new Date(),
+        //         nextSuggestedLearningDate: new Date(),
+        //         noMistakeInARow: 0
+        //     },
+        //     3: {
+        //         id: 3,
+        //         name: "lesson 3",
+        //         cardIds: [5,6],
+        //         lastLearningDate: new Date(),
+        //         nextSuggestedLearningDate: new Date(),
+        //         noMistakeInARow: 0
+        //     },
+        //     4: {
+        //         id: 4,
+        //         name: "lesson 4",
+        //         cardIds: [1,2,3,4,5,6],
+        //         lastLearningDate: new Date(),
+        //         nextSuggestedLearningDate: new Date(),
+        //         noMistakeInARow: 0
+        //     }
+        // }
     }
 }
